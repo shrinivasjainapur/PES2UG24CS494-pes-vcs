6dfe05df5199530be2f2a731959db535af912963
shrinivasjainapur <jainapurshrinivas@gmail.com>
2026-04-18 00:07:55 +0530

    Refactor main function for cleaner setup

--- test_objects.c
+++ test_objects.c
@@ -14,7 +14,6 @@
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out);
 int object_read(const ObjectID *id, ObjectType *type_out, void **data_out, size_t *len_out);
 int object_exists(const ObjectID *id);
-int has_parent;
 void object_path(const ObjectID *id, char *path_out, size_t path_size);
 
 void test_blob_storage(void) {
@@ -85,9 +84,8 @@ void test_integrity(void) {
 
 int main(void) {
     // Clean slate
-    int rc __attribute__((unused));
-    rc = system("rm -rf .pes");
-    rc = system("mkdir -p .pes/objects .pes/refs/heads");
+    system("rm -rf .pes");
+    system("mkdir -p .pes/objects .pes/refs/heads");
 
     test_blob_storage();
     test_deduplication();
