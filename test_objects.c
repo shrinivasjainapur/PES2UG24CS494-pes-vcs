981503b787d7eed0a31414f7fc6e167b82fe2693
shrinivasjainapur <jainapurshrinivas@gmail.com>
2026-04-17 22:16:42 +0530

    Update test_objects.c

--- test_objects.c
+++ test_objects.c
@@ -14,6 +14,7 @@
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out);
 int object_read(const ObjectID *id, ObjectType *type_out, void **data_out, size_t *len_out);
 int object_exists(const ObjectID *id);
+int has_parent;
 void object_path(const ObjectID *id, char *path_out, size_t path_size);
 
 void test_blob_storage(void) {
